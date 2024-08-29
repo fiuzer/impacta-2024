a58de9afecd443f18ac5d9ebc765ecd016f75860
Guilherme Fiuza <guigofiuza@gmail.com>
2024-08-29 13:55:07 -0300

    initial commit

---
+++ aula2.py
@@ -0,0 +1,11 @@
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+  print('Você entrou no sistema')
+
+elif entrada == 'sair':
+  print('Você saiu do sistemas')
+else:
+  print('Digite "entrar" ou "sair"')
+
+
