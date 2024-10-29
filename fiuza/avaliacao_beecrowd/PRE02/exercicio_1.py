b10e2e505ee9afcafa4c944c75efd7fc82f00fe2
Fiuza <91514791+fiuzer@users.noreply.github.com>
2024-10-29 19:31:09 -0300

    Update exercicio_1.py

--- fiuza/avaliacao_beecrowd/PRE02/exercicio_1.py
+++ fiuza/avaliacao_beecrowd/PRE02/exercicio_1.py
@@ -1,8 +1,11 @@
+# Pega as notas
 nota_trab = float(input())
 nota_prova = float(input())
+# Realiza a média
 media = (nota_trab + nota_prova) / 2
 nota_sub = 12 - nota_trab
 
+# Checa e apresenta qual é a situação do aluno
 if media >= 6:
   print("aprovado")
 elif nota_sub <= 10:
